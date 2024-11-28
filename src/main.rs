b20ade22f93ba5d6d8a9d687400c9102bcc6f735
Luciano Ramello <luchosr@gmail.com>
2024-11-28 15:32:00 +0100

    feat: implements build function

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,26 @@ mod my_lib;
 
 // crate o lib :: modulo:: funcion
 use my_lib::my_lib::run;
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config: Config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-    println!("With text:\n{contents}");
+    run(config);
+
+    fn run(config: Config) {
+        let contents =
+            fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+        println!("With text:\n{contents}");
+    }
 }
 
 struct Config {
@@ -20,9 +29,15 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
 
-    Config { query, file_path }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
