26b1f2a4d3c52f2c97309db330b98bf4a71463f5
Luciano Ramello <luchosr@gmail.com>
2024-11-28 16:07:28 +0100

    feat: refactor main crate

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-mod my_lib;
-
 // crate o lib :: modulo:: funcion
-use my_lib::my_lib::run;
-use std::{env, fs, process};
+use lib::{run, Config};
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,30 +12,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
-
-    fn run(config: Config) {
-        let contents =
-            fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-        println!("With text:\n{contents}");
-    }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
