df239c3370b858e295d8e32eb4bb0f52f58a1d1a
Luciano Ramello <luchosr@gmail.com>
2024-12-26 20:28:22 +0100

    feat: improve error handling

--- src/minigrep/lib.rs
+++ src/minigrep/lib.rs
@@ -40,6 +40,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
 
